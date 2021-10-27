29f3be30ff4eb196f441451c7decf16be3652aa9
soo <soo9262001@naver.com>
2021-10-27 12:49:12 +0900

    First Commit

--- TodoList/SceneDelegate.swift
+++ TodoList/SceneDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  SceneDelegate.swift
-//  TodoList
-//
-//  Created by joonwon lee on 2020/03/19.
-//  Copyright © 2020 com.joonwon. All rights reserved.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
